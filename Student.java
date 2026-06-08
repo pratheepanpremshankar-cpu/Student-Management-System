ca202a50e49e6985f303566ccae12276a5afae33
pratheepanpremshankar-cpu <pratheepanpremshankar@gmail.com>
2026-06-09 01:15:52 +0530

    Create Student class with attributes and constructor
    Create Student class

--- Student.java
+++ Student.java
@@ -1,4 +1,33 @@
 class Student
 {
+	int studentid;
+	String studentName;
+	int marks;
 	
+	public Student(int stuID , String stuName , int mrks)
+	{
+		studentid = stuID;
+		studentName = stuName;
+		marks = mrks;
+	}
+	
+	public String getName()
+	{
+		return studentName;
+	}
+	
+	public int getID()
+	{
+		return studentid;
+	}
+	
+	public int getMarks()
+	{
+		return marks;
+	}
+	
+	public String toString()
+	{
+		return "Student name "+studentName+"\nStudent id "+studentid+"\nMarks obtained "+marks;
+	}
 }
