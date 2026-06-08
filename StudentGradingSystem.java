a63153af768c71453be8bf221425bc7645c9a79d
pratheepanpremshankar-cpu <pratheepanpremshankar@gmail.com>
2026-06-09 01:26:43 +0530

    Accepts student ID, name, and marks , Creates Student objects from user input , Stores student records in an ArrayList
    Add student input functionality

--- StudentGradingSystem.java
+++ StudentGradingSystem.java
@@ -1,4 +1,15 @@
-class StudentGradingSystem
-{
-	
-}
+case 1:
+    print("Enter student ID: ");
+    int id = sc.nextInt();
+    sc.nextLine();
+
+    print("Enter student name: ");
+    String nme = sc.nextLine();
+
+    print("Enter the marks: ");
+    int mrks = sc.nextInt();
+    sc.nextLine();
+
+    student.add(new Student(id, nme, mrks));
+
+    break;
